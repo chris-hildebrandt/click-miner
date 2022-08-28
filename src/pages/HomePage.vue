395470cc6554a87ffa8a8eb224678e33cb305133
chris-hildebrandt <ch.hild15@gmail.com>
2022-08-28 09:46:36 -0600

    monster up

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="home flex-grow-1 d-flex flex-column align-items-center justify-content-center">
     <div class="home-card p-5 bg-white rounded elevation-3">
-      <img src="https://bcw.blob.core.windows.net/public/img/8600856373152463" alt="CodeWorks Logo" class="rounded-circle">
+      <div class="btn selectable selectable-none draggable-none" @click="slash">
+        <img src="https://pixeljoint.com/files/icons/full/skell1.gif" alt="CodeWorks Logo" class="rounded-circle monster">
+        <img class="slash" v-if="toggle" src="https://thumbs.gfycat.com/AridDigitalGibbon-max-1mb.gif" alt="CodeWorks Logo">
+      </div>
       <h1 class="my-5 bg-dark text-white p-3 rounded text-center">
         Vue 3 Starter
       </h1>
@@ -10,21 +13,48 @@
 </template>
 
 <script>
+import { computed } from "@vue/reactivity";
+import { AppState } from "../AppState.js";
+
+
 export default {
-  name: 'Home'
+  name: 'Home',
+  setup(){
+    return{
+      toggle: computed(()=> AppState.toggle),
+      slash(){
+        console.log('slash!');
+        AppState.toggle = !AppState.toggle
+        console.log(AppState.toggle);
+      }
+    }
+  }
+
 }
 </script>
 
 <style scoped lang="scss">
-.home{
+.home {
   display: grid;
   height: 80vh;
   place-content: center;
   text-align: center;
   user-select: none;
-  .home-card{
+
+  .home-card {
     width: 50vw;
-    > img{
+
+    .slash{
+      position: absolute;
+      width: 200px;
+      right: 0px;
+    }
+
+    .monster{
+      position: relative;
+    }
+
+    >img {
       height: 200px;
       max-width: 200px;
       width: 100%;
